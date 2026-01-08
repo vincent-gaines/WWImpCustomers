017c98a26aa39f8b734114612188dc75bde04b03
vincent-gaines <vincentanggaines@gmail.com>
2026-01-08 13:40:03 -0500

    yep

--- WWImpCustomers/Program.cs
+++ WWImpCustomers/Program.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WWImpCustomers.Data;
 using WWImpCustomers.Services;
 using WWImpCustomers.Startup;
 
@@ -15,15 +16,18 @@ namespace WWImpCustomers
         [STAThread]
         static void Main()
         {
-
-            DependencyInjection.Configure();
-            var provider = DependencyInjection.ServiceProvider;
+            DependencyInjection.Configure();  // initializes the static ServiceProvider
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            var form = new Form1(provider.GetRequiredService<ICustomerService>());
+            var form = new Form1(
+                DependencyInjection.ServiceProvider.GetRequiredService<ICustomerService>(),
+                DependencyInjection.ServiceProvider.GetRequiredService<ILookupRepository>()
+            );
+
             Application.Run(form);
         }
+
     }
 }
